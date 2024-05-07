b7955f016623d8a1b17975c53c1633dba82ea7ce
KB Choi <1mdifferent@naver.com>
2024-05-08 05:56:00 +0900

    로그인버튼 메인으로

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ import ProductDetail from "./page/ProductDetail";
 import Navbar from "./component/Navbar";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [authenticate, setAuthenticate] = useState(false);
+  const [authenticate, setAuthenticate] = useState(false); //true면 로그인이 됨
+  useEffect(() => {
+    console.log("aaa", authenticate);
+  }, [authenticate]);
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login setAuthenticate={setAuthenticate} />}
+        />
+        //setAuthenticate함수를 props로 보내는 것
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </div>
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Form, Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ setAuthenticate }) => {
+  //setAuthenticate함수 props로 받기
+  const navigate = useNavigate(); //로그인후 메인홈페이지로 돌아가기 위해서
   const loginUser = (event) => {
-    event.preventDefault(); //Form을 쓰게되면 event.preventDefault 사용은 필수!(새로고침이 되는걸 막아줌)
+    event.preventDefault(); //Form을 쓰게되면 event.preventDefault 사용은 필수!(로그인버튼 클릭되면 새로고침이 되는걸 막아줌)
     console.log("login user fuction issue");
+    setAuthenticate(true); //setAuthenticate true로 바꾸기
+    navigate("/"); //메인홈으로 돌아간다
   };
   return (
+    //container는 가운데로 모아주는 기능을 한다
     <Container>
       <Form onSubmit={(event) => loginUser(event)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
